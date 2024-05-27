686fbcddf6e84c6cf10d6e8909494c4e20dca077
zvig23 <dvirlevi95@gmail.com>
2024-05-28 00:43:20 +0300

    removed forebase

--- hall_of_fame_prototype/src/App.tsx
+++ hall_of_fame_prototype/src/App.tsx
@@ -1,21 +1,13 @@
 import { useState } from 'react'
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { collection, getDocs } from "firebase/firestore"; 
-
+import { collection, getDocs } from "firebase/firestore";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { firebaseConfig } from './config/firebase';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC8iKq3Bsid_xnIqeGUp8K_sr_hV9Da-bU",
-  authDomain: "halloffameprototype.firebaseapp.com",
-  projectId: "halloffameprototype",
-  storageBucket: "halloffameprototype.appspot.com",
-  messagingSenderId: "190473257813",
-  appId: "1:190473257813:web:515268cf8cade91d8fe5dc",
-  measurementId: "G-HK9YNM4EZ3"
-}
+
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
